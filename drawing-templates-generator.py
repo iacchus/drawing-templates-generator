bdc4e84ec2227f60679b82edb88a29bd3263f065
ingstudio <117122269+ingeniumstudio@users.noreply.github.com>
2025-09-19 00:12:22 -0300

    commit

--- drawing-templates-generator.py
+++ drawing-templates-generator.py
@@ -5,21 +5,22 @@ import glob
 import click
 
 from wand.image import Image
+from wand.image import PAPERSIZE_MAP
 from wand.drawing import Drawing
 from wand.color import Color
 
-@click.group(epilog="gen")
-def template_generator():
-    pass
+#  @click.group(epilog="gen")
+#  def template_generator():
+#      pass
 
-width = 1200
-height = 2000
-resolution = 288
-spacing = 11
-columns = 4
+#  width = 1200
+#  height = 2000
+#  resolution = 288
+#  spacing = 11
+#  columns = 4
 
-filename_prefix = "square"
-filename_extension = "png"
+#  filename_prefix = "square"
+#  filename_extension = "png"
 
 stroke_width = 2
 fill_color = Color("white")
@@ -76,6 +77,8 @@ def get_circle_geometry(x, y, spacing, square_size):
             )
 
     return geometry
+
+
 def get_square_size(page_size, spacing, columns):
     outer_margin = spacing * 2
     intermediary_total_padding = ((columns-1) * spacing)
@@ -97,30 +100,9 @@ def get_max_lines(page_height, spacing, square_size):
 
     return count - 1
 
-#  with Drawing() as draw:
-#      draw.fill_color = fill_color
-#      draw.stroke_color = stroke_color
-#      draw.stroke_width = stroke_width
-#
-#      square_size = get_square_size(page_size=width, spacing=spacing, columns=columns)
-#
-#      lines = get_max_lines(page_height=height, spacing=spacing, square_size=square_size)
-#
-#      squares = [(x+1, y+1) for x in range(columns) for y in range(lines)]
-#
-#      for square_x, square_y in squares:
-#          geometry = get_square_geometry(x=square_x, y=square_y, spacing=spacing, square_size=square_size)
-#          draw.rectangle(**geometry)
-#
-#
-#      with Image(width=width, height=height, background=background) as image:
-#          draw(image)
-#
-#          filename = create_filename(prefix=filename_prefix, extension=filename_extension)
-#          image.save(filename=filename)
-#          #  image.save(filename='square.png')
 
-def draw_and_write(shape="square", format="img"):
+def draw_and_write(width, height, spacing, columns, resolution, shape,
+                   file_format):
     with Drawing() as draw:
         draw.fill_color = fill_color
         draw.stroke_color = stroke_color
@@ -136,6 +118,7 @@ def draw_and_write(shape="square", format="img"):
             for square_x, square_y in squares:
                 geometry = get_square_geometry(x=square_x, y=square_y, spacing=spacing, square_size=square_size)
                 draw.rectangle(**geometry)
+
         elif shape == "circle":
 
             for square_x, square_y in squares:
@@ -148,39 +131,56 @@ def draw_and_write(shape="square", format="img"):
                    resolution=resolution) as image:
             draw(image)
 
-            filename = create_filename(prefix=filename_prefix, extension=filename_extension)
+            #  filename = create_filename(prefix=filename_prefix, extension=filename_extension)
+            filename = create_filename(prefix=shape, extension=file_format)
+
             image.save(filename=filename)
             #  image.save(filename='square.png')
-width_option = click.option("-w", "--width", type=int, default=1200)
-height_option = click.option("-h", "--height", type=int, default=2000)
-spacing_option = click.option("-s", "--spacing", type=int, default=33)
-columns_option = click.option("-c", "--columns", type=int, default= 4)
-page_option = click.option("-p", "--page", type=str, default="a4")  # "a4" etc... type is Choice
-dpi_option = click.option("-d", "--dpi", type=int, default=288)  # 72, 288 etc
-shape_option = click.option("-x", "--shape", type=click.Choice(["square", "circle"], case_sensitive=False))  # square or circle
+
+
+width_option = click.option("--width", type=int, default=1200)
+height_option = click.option("--height", type=int, default=2000)
+spacing_option = click.option("--spacing", type=int, default=33)
+columns_option = click.option("--columns", type=int, default=4)
+page_option = click.option("--page", type=str, default="a4")  # "a4" etc... type is Choice
+dpi_option = click.option("--dpi", type=int, default=288)  # 72, 288 etc
+#  shape_option = click.option("--shape", type=click.Choice(["square", "circle"], case_sensitive=False))  # square or circle
+square_option = click.option("--square", "shape", is_flag=True, flag_value="square", default="square")
+circle_option = click.option("--circle", "shape", is_flag=True, flag_value="circle")
+png_option = click.option("--png", "file_format", is_flag=True, flag_value="png", default="png")
+pdf_option = click.option("--pdf", "file_format", is_flag=True, flag_value="pdf")
 #  format_option = click.option()  # img or pdf
 
-@template_generator.command(epilog="generate image")
+#  @template_generator.command(epilog="generate image")
+
+@click.command(epilog="generate image")
 @width_option
 @height_option
 @spacing_option
 @columns_option
 @page_option
 @dpi_option
-@shape_option
-def img():
-    filename_prefix = "square"
-    filename_extension = "png"
+@square_option
+@circle_option
+@png_option
+@pdf_option
+def generate_template(width, height, spacing, columns, page, dpi, shape, file_format):
+    resolution_factor = dpi / 72
 
-@template_generator.command(epilog="generate pdf")
-@page_option
-@spacing_option
-@columns_option
-@dpi_option
-@shape_option
-def pdf():
-    filename_prefix = "square"
-    filename_extension = "pdf"
+    if file_format == "pdf":
+        page_w, page_h = PAPERSIZE_MAP[page]  # this size is for 72dpi
+        #  page_w, page_h = PAPERSIZE_MAP["a4"]  # this size is for 72dpi
+        width = page_w * resolution_factor
+        height = page_h * resolution_factor
+        #  width, height = (page_w * 4, page_h * 4)
 
-#  print("Squared template drawn and saved as", filename)
+    draw_and_write(width=width,
+                   height=height,
+                   spacing=spacing,
+                   columns=columns,
+                   resolution=dpi,
+                   shape=shape,
+                   file_format=file_format)
 
+
+generate_template()
